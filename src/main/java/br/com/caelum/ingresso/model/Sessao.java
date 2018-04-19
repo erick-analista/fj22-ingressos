3ebf46d9b467b56d72f6cbbc82f5a3e6280669ad
lab7488 <lab7488@caelumBSB03-18>
2018-04-18 21:09:32 -0300

    criado SalaController, atualizado SessaoDao, Sessao, SessaoForm

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,13 +2,32 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
+	public Sessao() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @dprecated hibernate only	
+	 */
 	public Sessao(LocalTime horario, Filme filme, Sala sala){
 		this.horario = horario;
 		this.filme = filme;
@@ -16,6 +35,30 @@ public class Sessao {
 	}
 	public LocalTime getHorarioTermino(){
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalTime getHorario() {
+		return horario;
+	}
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	public Filme getFilme() {
+		return filme;
+	}
+	public void setFilme(Filme filme) {
+		this.filme = filme;
 	}
 	
 	
